2c3d839ecb0ab755a641d8340240a83700751fc4
Cameron Devine <camerondtrain@gmail.com>
2018-12-28 09:49:36 -0800

    checking if I have access rights

--- assets/apiGateway-js-sdk/apigClient.js
+++ assets/apiGateway-js-sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://vb77csof10.execute-api.us-west-2.amazonaws.com/StateMint';
+    var invokeUrl = 'https://tj9mztctuk.execute-api.us-west-2.amazonaws.com/StateMint';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
---
+++ assets/js/tracking.js
@@ -0,0 +1,8 @@
+trackingID = 'UA-131371194-2';
+
+if (trackingID) {
+	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+	ga('create', trackingID, 'auto');
+	ga('set', 'anonymizeIp', true);
+	ga('send', 'pageview');
+}
